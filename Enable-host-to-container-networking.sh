b486f31b4e0332ccff33df29c30a9e718b6672d5
sarunas-zilinskas <54266037+sarunas-zilinskas@users.noreply.github.com>
2022-12-02 12:27:49 +0200

    Fix small typo

--- Enable-host-to-container-networking.sh
+++ Enable-host-to-container-networking.sh
@@ -5,9 +5,10 @@ NIC_NAME="eth0"
 DOCKER_ROUTING_INTERFACE_NAME="dockerrouteif"
 DOCKERNETWORK_IP_ADDRESS="192.168.0.249/32"
 DOCKERNETWORK_IP_RANGE="192.168.0.64/26"
+SLEEP_TIME="15" #seconds
 
-sleep 15 #Do not rush things if executing during boot. This line is not mandatory and can be removed.
+sleep ${SLEEP_TIME} #Do not rush things if executing during boot. This line is not mandatory and can be removed.
 
 
-ip link add ${DOCKER_ROUTING_INTERFACE_NAME} link ${NIC_NAME}type macvlan mode bridge ; ip addr add ${DOCKERNETWORK_IP_ADDRESS} dev ${DOCKER_ROUTING_INTERFACE_NAME} ; ip link set ${DOCKER_ROUTING_INTERFACE_NAME} up
+ip link add ${DOCKER_ROUTING_INTERFACE_NAME} link ${NIC_NAME} type macvlan mode bridge ; ip addr add ${DOCKERNETWORK_IP_ADDRESS} dev ${DOCKER_ROUTING_INTERFACE_NAME} ; ip link set ${DOCKER_ROUTING_INTERFACE_NAME} up
 ip route add ${DOCKERNETWORK_IP_RANGE} dev ${DOCKER_ROUTING_INTERFACE_NAME}
